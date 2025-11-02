5acbbf6517c4037b486d561087ed9d443353727e
Krishna-Teja1806 <tejak5471@gmail.com>
2025-11-02 11:59:44 +0530

    Create service-worker.js

---
+++ docs/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'smart-map-v2';
+const urlsToCache = [
+  'map.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
